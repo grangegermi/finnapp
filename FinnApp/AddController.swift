e11b99bb0f0b354f44ca2f915774954ad4b0d73e
MAC <macos@192.168.100.4>
2024-05-05 16:25:04 +0300

    third

--- FinnApp/AddController.swift
+++ FinnApp/AddController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import SnapKit
 
-class AddController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+class AddController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UITextFieldDelegate {
 
     var picker = UIPickerView()
     var textField = UITextField()
+    var dateText = UITextField()
     lazy var collectionView: UICollectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
@@ -24,28 +25,83 @@ class AddController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         view.addSubview(picker)
-        view.addSubview(textField)
+//        view.addSubview(textField)
         view.addSubview(collectionView)
         view.addSubview(dateTracker)
+        view.addSubview(dateText)
+        picker.delegate = self
+        picker.dataSource = self
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(Cell.self, forCellWithReuseIdentifier: Cell.id)
-        collectionView.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).inset(40)
-            make.left.equalTo(view.snp.left)
-            make.right.equalTo(view.snp.right)
-            make.bottom.equalTo(view.snp.bottom).inset(40)
+        textField.delegate = self
+        
+        picker.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).inset(20)
+            make.left.equalTo(view.snp.left).inset(40)
+            make.right.equalTo(view.snp.right).inset(40)
+//            make.bottom.equalTo(collectionView.snp.top).inset(20)
+            make.height.equalTo(view.snp.height).multipliedBy(0.1)
 
         }
+        
+//        textField.snp.makeConstraints { make in
+//            make.top.equalTo(picker.snp.bottom).inset(100)
+//            make.height.equalTo(view.snp.height).multipliedBy(0.1)
+////            make.width.equalTo(view.snp.width).multipliedBy(0.9)
+//            make.right.equalTo(view.snp.right)
+//            make.left.equalTo(view.snp.left)
+//        }
+        
+        dateText.snp.makeConstraints { make in
+            make.top.equalTo(collectionView.snp.bottom).inset(textField.bounds.height)
+            make.height.equalTo(30)
+            make.width.equalTo(view.snp.width).multipliedBy(0.9)
+        }
+        dateText.text = "fff"
+        dateText.borderStyle = .bezel
+        dateText.textColor = .black
+        dateText.inputView = dateTracker
+        
+        dateTracker.datePickerMode = .date
+        dateTracker.preferredDatePickerStyle = .wheels
+        picker.backgroundColor = .yellow
+        textField.borderStyle = .line
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: Cell.id)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).inset(100)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.height.equalTo(view.snp.height).multipliedBy(0.4)
+        }
+        
+        dateTracker.snp.makeConstraints { make in
+            make.top.equalTo(dateText.snp.bottom).inset(textField.bounds.height)
+            make.height.equalTo(view.snp.height).multipliedBy(0.3)
+            make.width.equalTo(view.snp.width).multipliedBy(0.7)
+            make.right.equalTo(view.snp.right).inset(40)
+            make.left.equalTo(view.snp.left).inset(40)
+        }
+        
+        
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 1
     }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return "Hello"
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
@@ -58,7 +114,7 @@ class AddController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         
         if indexPath.section == 0 {
             cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.id, for: indexPath) as! Cell
-            cell.imageOne.image = UIImage(systemName: "toilet")
+            cell.imageOne.image = UIImage(systemName: "paperplane.fill")
         }
         return cell
     }
@@ -78,10 +134,10 @@ class AddController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
 
         let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
         
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(0.2)), repeatingSubitem: item, count: 4)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(0.2)), repeatingSubitem: item, count: 6)
         
         let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 5
+        section.interGroupSpacing = 10
         
          return section
     }
